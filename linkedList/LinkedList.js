87c6e06ee93d8fb36979b15e7b270e1a09818e3e
andy <andy@molimama.cn>
2017-02-21 17:40:50 +0800

    'remove_func_in_linkedList'

--- linkedList/LinkedList.js
+++ linkedList/LinkedList.js
@@ -26,4 +26,21 @@ export class LinkedList(){
 			currNode = currNode.next;
 		}
 	}
+
+	findPrevious(item){
+		var currNode = this.head;
+		while(!(currNode.next == null) && (currNode.next.element != item)){
+			currNode = currNode.next;
+		}
+		return currNode;
+	}
+
+	remove(item){
+		var prevNode = this.findPrevious(item);
+		if(!(prevNode.next == null)){
+			prevNode.next = prevNode.next.next;
+		}
+	}
+
+
 }
--- test/linkedList_test.js
+++ test/linkedList_test.js
@@ -5,3 +5,12 @@ cities.insert('conway','head');
 cities.insert('Resel','conway');
 cities.insert('Alma','Resel');
 cities.display();
+
+var cities2 = new LinkedList();
+cities2.insert("Conway", "head");
+cities2.insert("Russellville", "Conway");
+cities2.insert("Carlisle", "Russellville");
+cities2.insert("Alma", "Carlisle");
+cities2.display();
+cities2.remove("Carlisle");
+cities2.display();
